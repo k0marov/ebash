a761ec3f259caf644759837f91a1d2178ef814f2
Sam Komarov <sam@skomarov.com>
2023-04-27 14:03:38 +0300

    fix the regexp and wrap echo arguments in quoutes

--- ebash.sh
+++ ebash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 function transpile {
-  echo $(cat $1) \
-	  | sed -E 's/\$(.\w)\+\+/\1=\$(( \$\1+1 ))/' # increment operator
+  echo "$(cat $1)" \
+	  | sed -E 's/\$([a-zA-Z_][a-zA-Z_0-9]*)\+\+/\1=\$(( \$\1+1 ))/' # increment operator
 }
 
 input_file=$1
@@ -11,8 +11,9 @@ if [[ -z $input_file ]]
 then
   echo "You should provide the input file as the first argument!"
 elif [[ ! -f $input_file ]] 
+then 
   echo "The input file $input_file does not exist!"
   exit 1
 fi
 
-echo $(transpile $input_file) | bash 
+echo "$(transpile $input_file)" | bash 
